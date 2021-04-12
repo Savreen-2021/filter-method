6160db559787c59d64d36a7bab48e61f7709ff70
Sales <Sales@AjaySharma-pc.qss-india.in>
2021-04-12 19:04:50 +0530

    filter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+// import AutoComplete from './AutoComplete'
+import {useState,useEffect} from 'react'
+function App () {
+  
+  const [api,setapi]=useState([]);
+  const [loading,setLoading] = useState(false);
+  const [search,setSearch] = useState('');
+  const [filterSearch,setFilterSearch] = useState([]);
 
-function App() {
+  useEffect( () => {
+     setLoading(true);
+    fetch( "https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json()) 
+    .then(data => 
+        {
+         setapi(data);
+         setLoading(false);
+        //  console.log(data);       
+     }
+    )
+    .catch(err=> console.log(err));
+
+} , []);
+
+useEffect(()=>{
+  setFilterSearch(
+    api.filter(user=>{
+      return user.name.toLowerCase().includes(search.toLowerCase())
+    })
+  )
+},[search,api])
+
+if(loading){
+  return<p>Loading countries.....</p>
+}
+
+// const filterUsers = api.filter(user=>{
+//   return user.name.toLowerCase().includes(search.toLowerCase())
+// })
+
+
   return (
+    <>
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>hllo</h1>
+      
+    <p>this is the demo project to try git</p>
+    {/* {search} */}
+     <input type="text" placeholder="Search" onChange={e=>setSearch(e.target.value)}/>  
+     {
+                        filterSearch.map( (value,index)=>{
+
+                            return(
+                              
+                                <tr> {value.name}&nbsp; &nbsp; &nbsp;{value.website} </tr>
+                                
+                            )
+
+                        } )
+                    }  
     </div>
+    {/* <AutoComplete/> */}
+    <div className="App">
+       <header className="App-header">
+         <h1>This is the heading</h1>
+       </header>
+     </div>
+
+    </>
   );
 }
 
